e8042c2f46e1f5976b9a37f2b144d50e17421f74
Fil <flip.04@mail.ru>
2022-09-20 13:17:05 +0300

    исправлена ошибка подсчета времени следующего повторения

--- domain/src/main/java/com/example/domain/utils/TimeConverter.kt
+++ domain/src/main/java/com/example/domain/utils/TimeConverter.kt
@@ -6,7 +6,7 @@ object TimeConverter {
     fun convertToMinutes(value: Long, type: TimeUnit) = when (type) {
         TimeUnit.MINUTES -> value
         TimeUnit.HOURS -> value * 60
-        TimeUnit.DAYS -> value * 24
+        TimeUnit.DAYS -> value * 24 * 60
         else -> throw IllegalArgumentException()
     }
 }
