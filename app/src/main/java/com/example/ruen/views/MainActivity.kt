06e4e3094d9c89f1743277f1bdcf7831d8f858aa
Fil <flip.04@mail.ru>
2022-09-20 13:10:13 +0300

    реализация смены выбора темы

--- app/src/main/java/com/example/ruen/views/MainActivity.kt
+++ app/src/main/java/com/example/ruen/views/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.ruen.views
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceManager
 import com.example.ruen.R
 import com.example.ruen.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
     private lateinit var binding: ActivityMainBinding
+    private val sharedPreference by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        checkAndSetTheme()
         setupNavigation()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sharedPreference.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sharedPreference.unregisterOnSharedPreferenceChangeListener(this)
     }
 
     private fun setupNavigation() = with(binding) {
@@ -29,16 +44,31 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.setupWithNavController(navController)
     }
 
-    private fun getStartFragmentId(): Int? {
-        val sharedPreference = PreferenceManager.getDefaultSharedPreferences(this)
-        return sharedPreference.getString(getString(R.string.preference_key_start_fragment), null)
-            ?.let {
-                when (it) {
-                    getString(R.string.setting_start_fragment_values_new_card) -> R.id.newCardFragment
-                    getString(R.string.setting_start_fragment_values_repeat) -> R.id.cardRepeatFragment
-                    getString(R.string.setting_start_fragment_values_cards) -> R.id.cardsFragment
-                    else -> throw UnsupportedOperationException("Unknown fragment for navigation graph")
-                }
+    private fun getStartFragmentId() =
+        sharedPreference.getString(getString(R.string.preference_key_start_fragment), null)?.let {
+            when (it) {
+                getString(R.string.setting_start_fragment_values_new_card) -> R.id.newCardFragment
+                getString(R.string.setting_start_fragment_values_repeat) -> R.id.cardRepeatFragment
+                getString(R.string.setting_start_fragment_values_cards) -> R.id.cardsFragment
+                else -> throw IllegalArgumentException("Unknown fragment for navigation graph")
             }
+        }
+
+    private fun checkAndSetTheme() {
+        sharedPreference.getString(getString(R.string.preference_key_theme), null)?.let {
+            val dayNightMode = when (it) {
+                getString(R.string.setting_theme_values_system) -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                getString(R.string.setting_theme_values_light) -> AppCompatDelegate.MODE_NIGHT_NO
+                getString(R.string.setting_theme_values_dark) -> AppCompatDelegate.MODE_NIGHT_YES
+                else -> throw IllegalArgumentException("Illegal theme argument")
+            }
+            AppCompatDelegate.setDefaultNightMode(dayNightMode)
+        }
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, name: String?) {
+        if (name == getString(R.string.preference_key_theme)) {
+            checkAndSetTheme()
+        }
     }
 }
