b7d576fc089706d577110c9df7869650b08a39a4
Fil <flip.04@mail.ru>
2022-09-27 19:34:19 +0300

    MainActivity.kt изменен под новый дизайн Перерисован экран fragment_card_repeat.xml

--- presentation/src/main/java/com/example/ruen/views/MainActivity.kt
+++ presentation/src/main/java/com/example/ruen/views/MainActivity.kt
@@ -1,25 +1,38 @@
 package com.example.ruen.views
 
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceManager
 import com.example.ruen.R
 import com.example.ruen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
     private lateinit var binding: ActivityMainBinding
+
+    private val navController by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navHostFragment.navController
+    }
     private val sharedPreference by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         checkAndSetTheme()
+        setupActionBar()
         setupNavigation()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 
     override fun onResume() {
@@ -32,16 +45,27 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
         sharedPreference.unregisterOnSharedPreferenceChangeListener(this)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> {
+                navController.navigate(R.id.action_cardRepeatFragment_to_settingFragment)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbar)
+    }
+
     private fun setupNavigation() = with(binding) {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navHostFragment.navController
         val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
         getStartFragmentId()?.let {
             navGraph.setStartDestination(it)
         }
         navController.graph = navGraph
-        bottomNavigationView.setupWithNavController(navController)
     }
 
     private fun getStartFragmentId() =
