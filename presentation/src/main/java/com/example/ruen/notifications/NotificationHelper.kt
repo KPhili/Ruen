25459223110506c8163d586e3dde29a712193640
Fil <flip.04@mail.ru>
2022-10-11 16:10:03 +0300

    иконка приложения

--- presentation/src/main/java/com/example/ruen/notifications/NotificationHelper.kt
+++ presentation/src/main/java/com/example/ruen/notifications/NotificationHelper.kt
@@ -32,12 +32,12 @@ class NotificationHelper(
     private fun getRepeatNotification() =
         NotificationCompat.Builder(context, REMIND_REPETITION_CHANNEL_ID)
             .setContentTitle(context.getString(R.string.time_to_repeat_the_word))
-            .setSmallIcon(R.drawable.circular_arrows).setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSmallIcon(R.drawable.ic_launcher_foreground).setPriority(NotificationCompat.PRIORITY_HIGH)
             .setContentIntent(getRepeatFragmentPendingIntent()).build()
 
     private fun getEmptyNotification() =
         NotificationCompat.Builder(context, REMIND_REPETITION_CHANNEL_ID)
-            .setSmallIcon(R.drawable.circular_arrows).setPriority(NotificationCompat.PRIORITY_LOW)
+            .setSmallIcon(R.drawable.ic_launcher_foreground).setPriority(NotificationCompat.PRIORITY_LOW)
             .build()
 
     private fun getRepeatFragmentPendingIntent(): PendingIntent {
